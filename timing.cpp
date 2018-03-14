0aa5b3512ddeb7af12298bc58ce615889afdd7c2
Jesper Sindahl Nielsen <jasn@cs.au.dk>
2018-03-14 11:44:51 +0100

    input generators for uniform and gauss mixture

--- timing.cpp
+++ timing.cpp
@@ -15,22 +15,83 @@
 using namespace std;
 
 static std::string datafile_name = "data.csv";
-static size_t start = 10000000;
+static size_t start = 1000000;
 static size_t increment = 10000000;
 
 static omp_lock_t lock;
 
+class input_generator {
+public:
+    explicit input_generator() {}
+    input_generator(const input_generator&) = delete;
+    input_generator& operator=(const input_generator&) = delete;
+    ~input_generator() = default;
+
+    virtual vector<double> generate(size_t n) = 0;
+};
+
+class input_generator_uniform : public input_generator {
+public:
+    explicit input_generator_uniform() {}
+    input_generator_uniform(const input_generator_uniform&) = delete;
+    input_generator_uniform& operator=(const input_generator_uniform&) = delete;
+    ~input_generator_uniform() = default;
+    vector<double> generate(size_t n) override {
+        mt19937 mt(time(0));
+        vector<double> res(n, 0);
+        double factor = 1e6;
+        double maximum = 0;
+        for (size_t i = 0; i < n; ++i) {
+            res[i] = mt();
+            maximum = std::max(maximum, res[i]);
+        }
+        std::sort(res.begin(), res.end());
+        for (auto &v : res) {
+            v = (v / maximum) * factor;
+        }
+        return res;
+    }
+};
+
+class input_generator_gauss_mixture : public input_generator {
+public:
+    explicit input_generator_gauss_mixture() {}
+    input_generator_gauss_mixture(const input_generator_gauss_mixture&) = delete;
+    input_generator_gauss_mixture& operator=(const input_generator_gauss_mixture&) = delete;
+    ~input_generator_gauss_mixture() = default;
+    vector<double> generate(size_t n) override {
+        size_t k = 16;
+        std::vector<double> centers(k, 0);
+        for (size_t i = 0; i < k; ++i) {
+            centers[i] = i * 1e6;
+        }
+        mt19937 mt(time(0));
+        std::normal_distribution<double> gauss(0.0, 100.0);
+        vector<double> res(n, 0);
+        double factor = 1e6;
+        double maximum = 0;
+        for (size_t i = 0; i < n; ++i) {
+            size_t cluster = mt() % k;
+            res[i] = centers[cluster] + gauss(mt);
+            maximum = std::max(maximum, res[i]);
+        }
+        std::sort(res.begin(), res.end());
+        return res;
+    }
+};
+
 vector<double> generate(size_t n) {
     mt19937 mt(time(0));
     vector<double> res(n, 0);
-    double maximum = 1;
+    double factor = 1e6;
+    double maximum = 0;
     for (size_t i = 0; i < n; ++i) {
         res[i] = mt();
         maximum = std::max(maximum, res[i]);
     }
     std::sort(res.begin(), res.end());
     for (auto &v : res) {
-        v = v / maximum;
+        v = (v / maximum) * factor;
     }
     return res;
 }
@@ -63,7 +124,7 @@ std::chrono::milliseconds time_compute_and_report(vector<double> &points, size_t
     return std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 }
 
-int main(int argc, char *argv[]) {
+int run(std::unique_ptr<input_generator> const &g, std::string outfilename) {
 
     //vector<size_t> ks = {1, 10, 50, 100, 500};
     vector<size_t> ks = {10, 20};
@@ -119,4 +180,23 @@ int main(int argc, char *argv[]) {
 
     }
     return 0;
+}
+
+int main(int argc, char* argv[]) {
+    std::unique_ptr<input_generator> generator(nullptr);
+    std::string outfilename;
+    if (argc > 1) {
+        if (argv[1] == "uniform") {
+            generator = std::move(std::unique_ptr<input_generator>(new input_generator_uniform()));
+            outfilename = datafile_name;
+        } else if (argv[1] == "gauss-mixture") {
+            generator = std::move(std::unique_ptr<input_generator>(new input_generator_gauss_mixture()));
+            outfilename = "timings_gauss.csv";
+        }
+    }
+    if (generator == nullptr) {
+        generator = std::unique_ptr<input_generator>(new input_generator_uniform());
+    }
+
+    run(generator, datafile_name);
 }
